2d57093e3e5caf49ead369f40c225d9fcf82b3d3
freeman <the.freeman.lab@gmail.com>
2016-08-27 10:38:04 -0700

    remove a few unneeded files

--- bench/clear.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (regl) {
-  return function () {
-    regl.clear({
-      color: [Math.random(), Math.random(), Math.random(), 1],
-      depth: 1,
-      stencil: 0
-    })
-  }
-}
